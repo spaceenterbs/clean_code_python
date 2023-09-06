1948478204d8f16da1d37ad590e6232051e61404
enterspacebs <anotherhell48@gmail.com>
2023-09-06 23:48:32 +0900

    zeroDivideTraceback.py added

--- abcTraceback.py
+++ abcTraceback.py
@@ -11,3 +11,23 @@ def c():
     42 / 0 # 이 식은 0으로 나누기 에러를 발생시킴
 
 a() # a() 함수 호출
+
+'''
+Start of a()
+Start of b()
+Start of c()
+Traceback (most recent call last):
+  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\abcTraceback.py", line 13, in <module>
+    a() # a() 함수 호출
+    ^^^
+  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\abcTraceback.py", line 3, in a
+    b() # b() 함수 호출
+    ^^^
+  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\abcTraceback.py", line 7, in b
+    c() # c() 함수 호출
+    ^^^
+  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\abcTraceback.py", line 11, in c
+    42 / 0 # 이 식은 0으로 나누기 에러를 발생시킴
+    ~~~^~~
+ZeroDivisionError: division by zero
+'''
---
+++ zeroDivideTraceback.py
@@ -0,0 +1,15 @@
+def spam(number1, number2):
+    return number1 / (number2 - 42)
+
+
+spam(101, 42)
+
+'''
+Traceback (most recent call last):
+  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 5, in <module>
+    spam(101, 42)
+  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 2, in spam
+    return number1 / (number2 - 42)
+           ~~~~~~~~^~~~~~~~~~~~~~~~
+ZeroDivisionError: division by zero
+'''
