4b60617c0a2026d2b5d3329da897ee08f45aacd3
enterspacebs <anotherhell48@gmail.com>
2023-09-07 17:28:02 +0900

    Path

--- zeroDivideTraceback.py
+++ zeroDivideTraceback.py
@@ -4,6 +4,8 @@ def spam(number1, number2):
 
 spam(101, 42)
 
+
+""" 단락 내부에 역슬래시가 들어가면 단락 주석이 안됨 """
 # Traceback (most recent call last):
 #   File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 5, in <module>
 #     spam(101, 42)
