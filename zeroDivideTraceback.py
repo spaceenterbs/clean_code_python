09de117beafa14074a738780694c5f56914a8469
enterspacebs <anotherhell48@gmail.com>
2023-09-07 16:31:28 +0900

    단락 주석 안됨

--- zeroDivideTraceback.py
+++ zeroDivideTraceback.py
@@ -4,12 +4,10 @@ def spam(number1, number2):
 
 spam(101, 42)
 
-'''
-Traceback (most recent call last):
-  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 5, in <module>
-    spam(101, 42)
-  File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 2, in spam
-    return number1 / (number2 - 42)
-           ~~~~~~~~^~~~~~~~~~~~~~~~
-ZeroDivisionError: division by zero
-'''
+# Traceback (most recent call last):
+#   File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 5, in <module>
+#     spam(101, 42)
+#   File "C:\Users\bangm\OneDrive\바탕 화면\clean_code_python\zeroDivideTraceback.py", line 2, in spam
+#     return number1 / (number2 - 42)
+#            ~~~~~~~~^~~~~~~~~~~~~~~~
+# ZeroDivisionError: division by zero
