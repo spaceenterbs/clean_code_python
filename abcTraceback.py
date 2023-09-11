19b2b8977fcf16912b90061a01be1812ca1d0e71
enterspacebs <anotherhell48@gmail.com>
2023-09-11 11:27:41 +0900

    lunch time

--- abcTraceback.py
+++ abcTraceback.py
@@ -1,16 +1,21 @@
+# def a():
+#     print("Start of a()")
+#     b()  # b() 함수 호출
+
+
+# def b():
+#     print("Start of b()")
+#     c()  # c() 함수 호출
+
+
+# def c():
+#     print("Start of c()")
+#     42 / 0  # 이 식은 0으로 나누기 에러를 발생시킴
+
+
 def a():
-    print("Start of a()")
-    b()  # b() 함수 호출
-
-
-def b():
-    print("Start of b()")
-    c()  # c() 함수 호출
-
-
-def c():
-    print("Start of c()")
-    42 / 0  # 이 식은 0으로 나누기 에러를 발생시킴
+    press = "press"
+    print(len(press))
 
 
 a()  # a() 함수 호출
